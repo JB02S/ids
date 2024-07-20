a949362441270f360bfbcd9655c44badcf6efbee
James Browne <the_real_one_7000@proton.me>
2024-07-20 11:41:52 +0100

    working on signature-based analysis to monitor for malicious traffic

--- src/main/java/tech/jamesabrowne/ids/signature/Signature.java
+++ src/main/java/tech/jamesabrowne/ids/signature/Signature.java
@@ -1,4 +1,16 @@
 package tech.jamesabrowne.ids.signature;
 
 public class Signature {
+
+    public Signature() {
+        
+    }
+    private int id;
+    private String pattern;
+    private String protocol;
+    private String srcIp;
+    private String dstIp;
+    private int srcPort;
+    private int dstPort;
+
 }
--- src/main/java/tech/jamesabrowne/ids/signature/SignatureLoader.java
+++ src/main/java/tech/jamesabrowne/ids/signature/SignatureLoader.java
@@ -1,4 +1,18 @@
 package tech.jamesabrowne.ids.signature;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.List;
+
 public class SignatureLoader {
+    public List<Signature> loadSignatures() throws Exception {
+        Gson gson = new Gson();
+        ClassLoader classLoader = getClass().getClassLoader();
+        try (Reader reader = new InputStreamReader(classLoader.getResourceAsStream("signatures.json"))) {
+            return gson.fromJson(reader, new TypeToken<List<Signature>>() {}.getType());
+        }
+    }
 }
